83f3f6cb1606dbc993c9145b46104fc47cdbc12f
xt1994 <2500409635@qq.com>
2016-12-07 17:24:10 +0800

    account-view

--- client/app/admin/account/account-view.controller.js
+++ client/app/admin/account/account-view.controller.js
@@ -1,25 +1,8 @@
-(function(){
+'use strict';
 
-  'use strict';
+angular.module('beiruixueApp')
+  .controller('AdminAccountViewCtrl', ['$scope', '$location', '$state','$stateParams','$cookieStore',
+    function ($scope, $location, $state,$stateParams,$cookieStore) {
 
-  angular
-    .module('beiruixueApp')
-    .controller('AdminAccountViewCtrl', adminAccountViewCtrl);
 
-  /* @ngInject */
-  function adminAccountViewCtrl($scope, $http, Auth, User) {
-
-    // Use the User $resource to fetch all users
-    $scope.users = User.query();
-
-    $scope.delete = function(user) {
-      User.remove({ id: user._id });
-      angular.forEach($scope.users, function(u, i) {
-        if (u === user) {
-          $scope.users.splice(i, 1);
-        }
-      });
-    };
-  };
-
-})();
+}]);
