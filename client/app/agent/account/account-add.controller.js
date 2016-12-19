b5bff269e2737e0eda00c5e6fc9e414bd1aa981d
xt1994 <2500409635@qq.com>
2016-12-19 16:14:34 +0800

    change

--- client/app/agent/account/account-add.controller.js
+++ client/app/agent/account/account-add.controller.js
@@ -3,28 +3,42 @@
 angular.module('beiruixueApp')
   .controller('AgentAccountAddCtrl', ['$scope', '$location', '$state','$stateParams','$cookieStore','User',
     function ($scope, $location, $state,$stateParams,$cookieStore,User) {
-	    var self = this;
+    var self = this;
 
-	    this.accountInfo = {
-	    	account:null,
-	    	name:null,
-	    	tel:null,
-	    	password:null,
-	    	rePassword:null,
-	    };
+    this.accountInfo = {
+    	account:null,
+    	name:null,
+    	tel:null,
+    	password:null,
+    	rePassword:null,
+    };
 
-	    self.create = function (){
-	    	if(!self.accountInfo.account || !self.accountInfo.name || !self.accountInfo.tel || !self.accountInfo.password){
-	    		return alert("请填写完整信息");
-	    	}
-	    	if(self.accountInfo.password != self.accountInfo.rePassword){
-	    		return alert("两次密码输入不同");
-	    	}
-	    	User.createMember({},self.accountInfo,function (data){
-	    		alert("创建成功！\n账号："+self.accountInfo.account+"\n密码："+self.accountInfo.password);
-	    		$state.go("agent-account-view");
-	    	},function(data){
-	    		alert(data);
-	    	});
-	    };
+	var system ={}; 
+    var p = navigator.platform;   
+    system.win = p.indexOf("Win") == 0; 
+    system.mac = p.indexOf("Mac") == 0; 
+    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);   
+    if(system.win||system.mac||system.xll){//如果是电脑跳转到百度 
+        // window.location.href="http://www.baidu.com/"; 
+        self.isPC = true;
+    }else{  //如果是手机,跳转到谷歌
+        // window.location.href="http://www.google.cn/"; 
+        self.isPC = false;
+        
+    }
+
+    self.create = function (){
+    	if(!self.accountInfo.account || !self.accountInfo.name || !self.accountInfo.tel || !self.accountInfo.password){
+    		return alert("请填写完整信息");
+    	}
+    	if(self.accountInfo.password != self.accountInfo.rePassword){
+    		return alert("两次密码输入不同");
+    	}
+    	User.createMember({},self.accountInfo,function (data){
+    		alert("创建成功！\n账号："+self.accountInfo.account+"\n密码："+self.accountInfo.password);
+    		$state.go("agent-account-view");
+    	},function(data){
+    		alert(data);
+    	});
+    };
 }]);
